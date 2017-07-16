d8f045445f2dec553f6cb6b4f320c138d75854b8
Smit Patel <smitpatel0510@gmail.com>
2017-07-17 03:03:42 +0530

    Implemented custom action bar

--- app/src/main/java/com/misfits/mental/quickread/MainActivity.java
+++ app/src/main/java/com/misfits/mental/quickread/MainActivity.java
@@ -1,13 +1,26 @@
 package com.misfits.mental.quickread;
 
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Gravity;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar)findViewById(R.id.toolbar_top);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+    }
+    public void openDrawer(View view)
+    {
+        DrawerLayout drawerLayout = (DrawerLayout)findViewById(R.id.drawer_layout);
+        drawerLayout.openDrawer(Gravity.START);
     }
 }
